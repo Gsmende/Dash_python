06ce00fffbbbe072088a5707bf837a65352cc11d
Guilherme Mendes <guilhermesouzamds@gmail.com>
2021-10-11 13:38:09 -0300

    plotly_dash

---
+++ data_generation.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+#df1 = pd.read_csv("./database/HIST_PAINEL_COVIDBR_2020_Parte1_21set2021.csv", sep=";")
+#df2 = pd.read_csv("./database/HIST_PAINEL_COVIDBR_2020_Parte2_21set2021.csv", sep=";")
+#df3 = pd.read_csv("./database/HIST_PAINEL_COVIDBR_2021_Parte1_21set2021.csv", sep=";")
+#df4 = pd.read_csv("./database/HIST_PAINEL_COVIDBR_2021_Parte2_21set2021.csv", sep=";")
+#database = pd.concat([df1, df2, df3, df4], ignore_index=True)
+#database.to_csv("./database/database.csv")
+
+#df = pd.read_csv("./database/database.csv")
+#df_states = df[(~df["estado"].isna()) & (df["codmun"].isna())]
+#df_brasil = df[df["regiao"] == "Brasil"]
+#df_states.to_csv("./database/df_states.csv")
+#df_brasil.to_csv("./database/df_brasil.csv")
